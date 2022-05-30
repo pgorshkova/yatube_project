0a3923a70ce35e999c63effb71ada8705d3c589a
Полина Горшкова <polina_gorshkova_0202@mail.ru>
2022-05-30 17:19:53 +0300

    Добавлены urls и view-функции

---
+++ yatube/posts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+def group_posts(request):
+    return HttpResponse(f'Посты по теме')
+# Create your views here.
